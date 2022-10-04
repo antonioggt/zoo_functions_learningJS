da351ff464fe4282db2aae7d4bafafaf3bf65740
antonioggt <antonioggt85@gmail.com>
2022-10-04 19:08:07 -0300

    resolve o requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,13 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('ao receber o parametro "count" retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count').toBe(4));
+  });
+  it('ao receber o parametro "names" retorna um array com a relação dos nomes de todos os elefantes', () => {
+    expect(handlerElephants('names').toContain('Jefferson'));
+  });
+  it('ao receber o parametro "averageAge" retorna a média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge').toBeCloseTo(10.5));
+  });
+});
