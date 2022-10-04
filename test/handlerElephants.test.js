0fc37b0c3970bf5595ee32938d360f0e3c8c088d
antonioggt <antonioggt85@gmail.com>
2022-10-04 19:56:11 -0300

    resolve o requisito 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -10,4 +10,16 @@ describe('Testes da função HandlerElephants', () => {
   it('ao receber o parametro "averageAge" retorna a média de idade dos elefantes', () => {
     expect(handlerElephants('averageAge').toBeCloseTo(10.5));
   });
+  it('ao receber o parametro "location" retorna a localização dos elefantes dentro do Zoológico', () => {
+    expect(handlerElephants('location').toBe('NW'));
+  });
+  it('ao receber o parametro "popularity" retorna um número igual ou maior a 5', () => {
+    expect(handlerElephants('popularity').toBeGreaterThanOrEqual(5));
+  });
+  it('ao receber o parametro "availability" retorna um array de dias da semana que não contém Monday', () => {
+    expect(handlerElephants('availability').not.toContain('Monday'));
+  });
+  it('Quando não passar parâmetro algum deve retornar undefined', () => {
+    expect(handlerElephants().toBe(undefined));
+  });
 });
